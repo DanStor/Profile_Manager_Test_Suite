2911ca7991f075eb53c0c1e3a782ef4f5128ec97
Michael Weller <vve11ermichae1@gmail.com>
2018-11-13 15:42:22 +0000

    Started Generic step defs

--- features/step_defs/generic_step_defs.rb
+++ features/step_defs/generic_step_defs.rb
@@ -1,13 +1,17 @@
 Given("I am typing an entry") do
-  PMTesting.generic.inputText 
+  generic.visitLoginPage
+  generic.logInAsAdmin
+  generic.visitSections
+  generic.clickOnNewSection
 end
 
 When("I am typing an entry which is larger than the text field") do
-  pending # Write code here that turns the phrase above into concrete actions
+  generic.inputTextinNewSection "Hobbies"
 end
 
 Then("I should be able to view the what I have typed") do
-  pending # Write code here that turns the phrase above into concrete actions
+  # expect(generic.checkIfTitlePresent).to eq true
+  # sleep 3
 end
 
 Given("I am logged in as a user") do
