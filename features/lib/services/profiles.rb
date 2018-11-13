8bfec733146a5c47d2e5ef19232ca55d3f4e8c21
tech-a49 <tom.e.beckett@gmail.com>
2018-11-13 15:38:49 +0000

    added some tests to profiles

--- features/lib/services/profiles.rb
+++ features/lib/services/profiles.rb
@@ -2,4 +2,19 @@ require "capybara"
 
 class Profiles
 
+  NEW_BUTTON_ID = 'new-button'
+  SUMMARY_FIELD_ID = 'profile-form-summary'
+
+  def visit_profile_page
+    visit('/profiles')
+  end
+
+  def click_create_profile
+    click_on(NEW_BUTTON_ID)
+  end
+
+  def enter_summary summary
+    fill_in(SUMMARY_FIELD_ID, with: summary)
+
+  end
 end
