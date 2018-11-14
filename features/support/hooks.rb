d057187eb83e76473ae66b994769ac6419b2f748
tech-a44 <robin.mck11@hotmail.co.uk>
2018-11-14 11:35:17 +0000

    Working on certifications scenarios

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,6 +1,21 @@
 Before("@certification") do
   visit('/')
   sign_in.sign_in_student
+
+  @certificate_added_message = "*Certification was successfully created."
+  sleep 1
+end
+
+Before("@certification_invalid_description") do
+  visit('/')
+  sign_in.sign_in_student
+
+  @certificate_added_message = "*Certification was successfully created."
+  sleep 1
+end
+
+
+After("@certification") do
   sleep 2
 end
 
