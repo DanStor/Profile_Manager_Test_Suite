24824cfd815ed35a13b1ab8c6d07d4c5f16c4725
Arun Kalwan <aruns1@hotmail.co.uk>
2018-11-15 10:56:33 +0000

    Completed Scenario 1 to be passed to other team members

--- features/lib/services/skills.rb
+++ features/lib/services/skills.rb
@@ -4,11 +4,33 @@ class Skills
   include Capybara::DSL
 
   # Scenario 1
+  def visit_homepage
+    visit('/')
+  end
 
+  def click_skills_tab
+    click_link('Skills')
+  end
+
+  def create_new_skill
+    click_link('Create Skill')
+  end
+
+  def fill_in_skill
+    fill_in('skill[skills]', :with => 'Team Leader')
+  end
+
+  def click_save
+    click_on('Save')
+  end
+
+  def get_notice
+    find('.notice').text
+  end
 
   # Scenario 2
 
-  
+
   # Scenario 3
 
 end
