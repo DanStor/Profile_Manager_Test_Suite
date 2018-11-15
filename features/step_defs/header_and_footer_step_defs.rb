b3f9079a8e6bd043cab33a16300c998e94b57026
Elisha <eharmison@spartaglobal.com>
2018-11-15 15:51:12 +0000

    scenario 1 complete - scenario 2 defs started

--- features/step_defs/header_and_footer_step_defs.rb
+++ features/step_defs/header_and_footer_step_defs.rb
@@ -1,55 +1,55 @@
 # Scenario 1
 Given("I am not logged in") do
-  pending # Write code here that turns the phrase above into concrete actions
+  header_and_footer.log_in_page
 end
 
 When("I click on the left sparta sign") do
-  pending # Write code here that turns the phrase above into concrete actions
+  header_and_footer.sparta_global_sign
 end
 
 When("I click on the skills tab") do
-  pending # Write code here that turns the phrase above into concrete actions
+  header_and_footer.click_skills
 end
 
 When("I click on the projects tab") do
-  pending # Write code here that turns the phrase above into concrete actions
+  header_and_footer.click_projects
 end
 
 When("I click on the employement tab") do
-  pending # Write code here that turns the phrase above into concrete actions
+  header_and_footer.click_employment
 end
 
 When("I click on the education tab") do
-  pending # Write code here that turns the phrase above into concrete actions
+  header_and_footer.click_education
 end
 
 When("I click on the certifications tab") do
-  pending # Write code here that turns the phrase above into concrete actions
+  header_and_footer.click_certifications
 end
 
 When("I click on the custom sections tab") do
-  pending # Write code here that turns the phrase above into concrete actions
+  header_and_footer.click_custom_sections
 end
 
 When("I click on the log in tab") do
-  pending # Write code here that turns the phrase above into concrete actions
+  header_and_footer.click_log_in
 end
 
 Then("I will be redirected back to the login page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  header_and_footer.log_in_page
 end
 
 # Scenario 2
 Given("I am logged in") do
-  pending # Write code here that turns the phrase above into concrete actions
+  header_and_footer.check_logged_in
 end
 
 When("I click the left top sparta sign") do
-  pending # Write code here that turns the phrase above into concrete actions
+  header_and_footer.sparta_global_sign_click
 end
 
 Then("I am directed to the profiles page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(header_and_footer.sparta_global_sign).to eq "http://localhost:3000/profiles"
 end
 
 # Scenario 3
@@ -92,129 +92,129 @@ end
 Then("I am directed to the educations page") do
   pending # Write code here that turns the phrase above into concrete actions
 end
-
-# Scenario 8
-When("I click the certifications tab") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I am directed to the certifications page") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-# Scenario 9
-When("I click the customs section tab") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I am directed to the customs section page") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-# Scenario 10
-When("I click the welcome username tab") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-# Scenario 11
-When("I click the logout tab") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I am directed to the login page") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-# Scenario 12
-Given("I am on the page") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When("I click on the bottom left sparta logo") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I will be redirected to the sparta global site") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-# Scenario 13
-When("I click on the bottom instagram logo") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I will be redirected to the sparta global instagram site") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-# Scenario 14
-When("I click on the bottom twitter logo") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I will be redirected to the sparta global twitter site") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-# Scenario 15
-When("I click on the bottom linkedin logo") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I will be redirected to the sparta global linkedin site") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-# Scenario 16
-When("I click on the bottom facebook logo") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I will be redirected to the sparta global facebook site") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-# Scenario 17
-When("I click on the bottom github logo") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I will be redirected to the sparta global github site") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-# Scenario 18
-Given("I am logged in as an admin account") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When("I click the top left sparta logo") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-# Scenario 20
-When("I click the sections tab") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I am directed to the sections page") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-# Scenario 21
-When("I click the teams tab") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I am directed to the teams page") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-# Scenario 22
-When("I click the bottom links") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I am directed to the http:\/\/localhost:{int}\/profiles\/{int}\/edit page") do |int, int2|
-  pending # Write code here that turns the phrase above into concrete actions
-end
+#
+# # Scenario 8
+# When("I click the certifications tab") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then("I am directed to the certifications page") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# # Scenario 9
+# When("I click the customs section tab") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then("I am directed to the customs section page") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# # Scenario 10
+# When("I click the welcome username tab") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# # Scenario 11
+# When("I click the logout tab") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then("I am directed to the login page") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# # Scenario 12
+# Given("I am on the page") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# When("I click on the bottom left sparta logo") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then("I will be redirected to the sparta global site") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# # Scenario 13
+# When("I click on the bottom instagram logo") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then("I will be redirected to the sparta global instagram site") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# # Scenario 14
+# When("I click on the bottom twitter logo") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then("I will be redirected to the sparta global twitter site") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# # Scenario 15
+# When("I click on the bottom linkedin logo") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then("I will be redirected to the sparta global linkedin site") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# # Scenario 16
+# When("I click on the bottom facebook logo") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then("I will be redirected to the sparta global facebook site") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# # Scenario 17
+# When("I click on the bottom github logo") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then("I will be redirected to the sparta global github site") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# # Scenario 18
+# Given("I am logged in as an admin account") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# When("I click the top left sparta logo") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# # Scenario 20
+# When("I click the sections tab") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then("I am directed to the sections page") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# # Scenario 21
+# When("I click the teams tab") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then("I am directed to the teams page") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# # Scenario 22
+# When("I click the bottom links") do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then("I am directed to the http:\/\/localhost:{int}\/profiles\/{int}\/edit page") do |int, int2|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
