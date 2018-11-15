bdfb6730a1974cc9a72d9fc9c0adbe277e60efef
tech-a50 <ratheaker@hotmail.com>
2018-11-15 10:36:05 +0000

    tests that should be passing/failing are doing so correctly. Now time to re-consider how the tests are run in order to make sure they may be run in any given circumstances

--- features/step_defs/employment_history_step_defs.rb
+++ features/step_defs/employment_history_step_defs.rb
@@ -4,11 +4,11 @@ Given("I am on the employment page") do
 end
 
 When("I click on an item") do
-  click_link("This Business")
+  click_link("A Business")
 end
 
 Then("I am now on the show page for that specific item") do
-  expect(find_by_id("company").value).to eq "This Business"
+  expect(find_by_id("company").value).to eq "A Business"
 end
 
 When("I click the new employment button") do
@@ -91,65 +91,71 @@ When("I click the back button") do
 end
 
 Then("I should not see a new item") do
-  pending # Write code here that turns the phrase above into concrete actions
+
 end
 
-When("I press the destroy button for a specific item") do
-  pending # Write code here that turns the phrase above into concrete actions
+When("I click the destroy button for a specific item") do
+  sleep 2
+  click_on("Destroy")
 end
 
-When("I click the confirm button on the delete entry alert") do
-  pending # Write code here that turns the phrase above into concrete actions
+When("I press the confirm button on the delete entry alert") do
+  click_on("Confirm")
 end
 
 Then("the targeted item should no longer be displayed on the employment page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(has_link?("A Business")).to be false
 end
 
 Then("I see a successfully destroyed message") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(find(".notice").text).to eq "*Employment was successfully destroyed."
 end
 
-When("I click the cancel button on the delete entry alert") do
-  pending # Write code here that turns the phrase above into concrete actions
+When("I press the cancel button on the delete entry alert") do
+  click_on("Cancel")
 end
 
 Then("the targeted item remains on the employment page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(has_link?("A Business")).to be true
 end
 
 Given("I am on the edit page for an employment item") do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit("http://localhost:3000/employments/1/edit")
+  sleep 2
+  expect(current_url).to eq "http://localhost:3000/employments/1/edit"
 end
 
 Then("I see the employment was successfully updated message") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(find(".notice").text).to eq "*Employment was successfully updated."
 end
 
 Given("I am on the new employment form page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit("http://localhost:3000/employments/new")
+  expect(current_url).to eq "http://localhost:3000/employments/new"
 end
 
 When("I click the currently employed checkbox") do
-  pending # Write code here that turns the phrase above into concrete actions
+  find("#still-employed").click
 end
 
 Then("the currently employed checkbox should be selected") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(find("#still-employed")).to be_checked
 end
 
 When("I edit the end date to a date after the current date") do
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in "end-date", with: "11/11/2021"
 end
 
 Then("the end date should display the date that was just input") do
-  pending # Write code here that turns the phrase above into concrete actions
+  content = find(".content-main").find_all("p")
+  expect(content[1].text).to eq "End date: 2021-11-11"
+
 end
 
 When("the still employed button has not been clicked") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(find("#still-employed")).not_to be_checked
 end
 
-Then("I should not be able to edit the end date to a date after the current date") do
-  pending # Write code here that turns the phrase above into concrete actions
+Then("I should see an invalid date error message") do
+  expect(find("#error_explanation").find("h2").text).to eq "1 error prohibited this employment from being saved:"
 end
--- features/teamLightningSharks/employment_history.feature
+++ features/teamLightningSharks/employment_history.feature
@@ -1,13 +1,5 @@
 Feature:  Employment History
 
-
-  @danRhys
-  Scenario: PM-191 - As a user, I must be able to view employment history item
-
-  Given I am on the employment page
-  When I click on an item
-  Then I am now on the show page for that specific item
-
   @danRhys
   Scenario: PM-192 As a user, I must be able to create a new employment history item
 
@@ -30,21 +22,14 @@ Feature:  Employment History
   When I click the back button
   Then I should not see a new item
 
-  # @danRhys
-  Scenario: PM-193 As a user, I must be able to delete an employment history item
+  @danRhys
+  Scenario: PM-191 - As a user, I must be able to view employment history item
 
   Given I am on the employment page
-  When I press the destroy button for a specific item
-  And I click the confirm button on the delete entry alert
-  Then the targeted item should no longer be displayed on the employment page
-  And I see a successfully destroyed message
+  When I click on an item
+  Then I am now on the show page for that specific item
 
-  Given I am on the employment page
-  When I press the destroy button for a specific item
-  And I click the cancel button on the delete entry alert
-  Then the targeted item remains on the employment page
-
-  # @danRhys
+  @danRhys
   Scenario: PM-194 As a user, I must be able to edit an employment history item
 
   Given I am on the edit page for an employment item
@@ -53,7 +38,7 @@ Feature:  Employment History
   Then I am redirected to the employment show page
   And I see the employment was successfully updated message
 
-  # @danRhys
+  @danRhys
   Scenario: PM-196 As a user, when I am adding employment history I should be able to select an option that I am currently employed.
 
   Given I am on the new employment form page
@@ -61,7 +46,7 @@ Feature:  Employment History
   And I click the currently employed checkbox
   Then the currently employed checkbox should be selected
 
-  # @danRhys
+  @danRhys
   Scenario: PM-202 As a user, I shouldn't be able to put a date after the current date, in the employments page, unless I check the still employed box.
 
   Given I am on the new employment form page
@@ -69,12 +54,31 @@ Feature:  Employment History
   And I click the currently employed checkbox
   And I edit the end date to a date after the current date
   # Fails at this stage as the end date input field becomes untargetable
+  And I click the save button
+  And I click the back button
+  And I click on an item
   Then the end date should display the date that was just input
 
-  # @danRhys
+  @danRhys
   Scenario: PM-203 Failed Test - User is able to enter a data after the current date when the still employed option is not clicked, in the employments page
 
   Given I am on the new employment form page
   When I enter valid details
   And the still employed button has not been clicked
-  Then I should not be able to edit the end date to a date after the current date
+  And I edit the end date to a date after the current date
+  And I click the save button
+  Then I should see an invalid date error message
+
+  @danRhys
+  Scenario: PM-193 As a user, I must be able to delete an employment history item
+
+  Given I am on the employment page
+  When I click the destroy button for a specific item
+  And I press the cancel button on the delete entry alert
+  Then the targeted item remains on the employment page
+
+  Given I am on the employment page
+  When I click the destroy button for a specific item
+  And I press the confirm button on the delete entry alert
+  Then the targeted item should no longer be displayed on the employment page
+  And I see a successfully destroyed message
