e7156bcbf83a0b7373b0a6ee0701172dbcb11b5b
Daniel Storey <dstorey@spartaglobal.com>
2018-11-19 12:09:09 +0000

    Admin 6 & 7 corrected

--- features/lib/services/admin.rb
+++ features/lib/services/admin.rb
@@ -96,18 +96,14 @@ class Admin
     select('Pending', :from => 'status')
   end
 
-  def show_pending_profiles
+  def shows_only_pending_profiles?
     within "#approved" do
-      if has_css?(".Approved")
+      if has_css?(".Approved") || has_css?(".Rejected") || has_css?(".Draft")
         return false
-      elsif has_css?(".Rejected")
-        return false
-      elsif has_css?(".Draft")
-        return false
-      else (".Pending")
-        return true
       end
     end
+
+    return true
   end
 
 # Scenario 10
@@ -160,16 +156,12 @@ class Admin
 
   def isOnlyDraft
     within "#approved" do
-      if has_css?(".Approved")
+      if has_css?(".Approved") || has_css?(".Rejected") || has_css?(".Pending")
         return false
-      elsif has_css?(".Rejected")
-        return false
-      elsif has_css?(".Pending")
-        return false
-      elsif has_css?(".Draft")
-        return true
       end
     end
+    
+    return true
   end
 
 # Scenario 8
